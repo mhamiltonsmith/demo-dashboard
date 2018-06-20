686a6de5a9c906b20f0bee87ca40131c8552fc62
mhamiltonsmith@runbox.com <mhamiltonsmith@runbox.com>
2018-06-20 13:50:05 +0000

    Changed icons and file structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [
-    '../assets/lib/material-design-icons/css/material-design-iconic-font.min.css',
     './app.component.css'
   ]
 })
